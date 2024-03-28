a310a99519a8bd9ed50651312a7a9389d687ce54
shonentropy3 <qwerdfkkt@163.com>
2024-03-28 20:58:53 +0800

    grapg

---
+++ src/merge-token-portal.ts
@@ -0,0 +1,104 @@
+import {
+  DepositToMerge as DepositToMergeEvent,
+  WithdrawFromMerge as WithdrawFromMergeEvent
+} from "../generated/mergeTokenPortal/mergeTokenPortal";
+import {
+  DepositToMerge,
+  WithdrawFromMerge
+} from "../generated/schema";
+import { Balance, TotalBalance } from "../generated/schema";
+import { BigInt, Bytes, crypto } from "@graphprotocol/graph-ts";
+
+const BIGINT_ZERO = BigInt.fromI32(0);
+export function handleDepositToMerge(event: DepositToMergeEvent): void {
+  let entity = new DepositToMerge(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.sourceToken = event.params.sourceToken
+  entity.mergeToken = event.params.mergeToken
+  entity.sender = event.params.sender
+  entity.amount = event.params.amount  
+  entity.receiver = event.params.receiver
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  const balance = loadOrCreateBalance(entity.sourceToken, entity.mergeToken, entity.blockTimestamp, entity.amount);
+  const totalBalance = loadOrCreateTotalBalance(entity.sourceToken, entity.mergeToken);
+  balance.sourceToken = entity.sourceToken;
+  balance.mergeToken = entity.mergeToken;
+  totalBalance.sourceToken = entity.sourceToken;
+  totalBalance.mergeToken = entity.mergeToken;
+  balance.balance = balance.balance.plus(entity.amount);
+  totalBalance.totalBalance = totalBalance.totalBalance.plus(entity.amount);
+  balance.save();
+  }
+
+export function handleWithdrawFromMerge(event: WithdrawFromMergeEvent): void {
+  let entity = new WithdrawFromMerge(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.sourceToken = event.params.sourceToken
+  entity.mergeToken = event.params.mergeToken
+  entity.sender = event.params.sender
+  entity.amount = event.params.amount
+  entity.receiver = event.params.receiver
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  const balance = loadOrCreateBalance(entity.sourceToken, entity.mergeToken, entity.blockTimestamp, entity.amount);
+  const totalBalance = loadOrCreateTotalBalance(entity.sourceToken, entity.mergeToken);
+  balance.sourceToken = entity.sourceToken;
+  balance.mergeToken = entity.mergeToken;
+  totalBalance.sourceToken = entity.sourceToken;
+  totalBalance.mergeToken = entity.mergeToken;
+  balance.balance = balance.balance.div(entity.amount);
+  totalBalance.totalBalance = totalBalance.totalBalance.div(entity.amount);
+  balance.save();
+}
+
+export function loadOrCreateBalance(sourceToken: Bytes, mergeToken: Bytes, blockTimestamp: BigInt, initBalance: BigInt): Balance {
+  sourceToken = toLowerCase(sourceToken);
+  mergeToken = toLowerCase(mergeToken);
+  const id = Bytes.fromByteArray(
+    crypto.keccak256(sourceToken.concat(mergeToken).concat(Bytes.fromBigInt(blockTimestamp)))
+  );
+  let balance = Balance.load(id);
+
+  if (!balance) {
+    balance = new Balance(id);
+    balance.sourceToken = sourceToken;
+    balance.balance = initBalance;
+    balance.mergeToken = mergeToken;
+    balance.blockTimestamp = BIGINT_ZERO;
+    balance.save();
+  }
+  return balance;
+}
+
+export function loadOrCreateTotalBalance(sourceToken: Bytes, mergeToken: Bytes): TotalBalance {
+  sourceToken = toLowerCase(sourceToken);
+  mergeToken = toLowerCase(mergeToken);
+  const id = Bytes.fromByteArray(
+    crypto.keccak256(sourceToken.concat(mergeToken))
+  );
+  let totalBalance = TotalBalance.load(id);
+
+  if (!totalBalance) {
+    totalBalance = new TotalBalance(id);
+    totalBalance.sourceToken = sourceToken;
+    totalBalance.totalBalance = BIGINT_ZERO;
+    totalBalance.mergeToken = mergeToken;
+    totalBalance.save();
+  }
+  return totalBalance;
+}
+
+export function toLowerCase(address: Bytes): Bytes {
+  return Bytes.fromHexString(address.toHexString().toLowerCase());
+}
+
+
